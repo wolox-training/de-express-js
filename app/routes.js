b57abd419949df68a317fc61e6481f158a42af19
Daniel Estrada <dannielestrada@MacBook-Pro-de-Wolox.local>
2020-11-18 14:24:30 -0500

    Updating of libraries for compatibility with node 14

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,10 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
+const user = require('./controllers/user');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.post('/users', user.singUp);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
